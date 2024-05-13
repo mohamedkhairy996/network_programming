6d3ab257d147acdeec60fdeb7643e4e2e71f7da8
Mohamed Khairy <91643324+mohamedkhairy996@users.noreply.github.com>
2024-05-13 21:59:10 +0300

    Update client.py

--- Task-1/client.py
+++ Task-1/client.py
@@ -12,35 +12,27 @@ server_ip = '127.0.0.1'
 server_port = 7000
 
 chat.connect((server_ip, server_port))
-
-def receive(chat):
-    while True:
-        try:
-            server_message = chat.recv(2048).decode('UTF-8')
-            if not server_message:
-                break
-            print('Server message:', server_message)
-        except ConnectionResetError:
-            print("Connection closed by server.")
-            break
-
-receive_thread = th.Thread(target=receive, args=(chat,))
-receive_thread.start()
-
 while True:
-    message = input('Send: ')
-    if message == 'close':
-        chat.send(message.encode('UTF-8'))
+    x = input('Client : ')
+    if x == 'q':
+        print('connection closed')
+        chat.send(x.encode('utf-8'))
         break
-    else:
-        total_sent = 0
-        message_bytes = message.encode('UTF-8')
-        message_length = len(message_bytes)
-        while total_sent < message_length:
-            sent = chat.send(message_bytes[total_sent:])
-            if sent == 0:
-                print("Connection closed by server.")
-                break
-            total_sent += sent
+
+    # Send the length of the message (fixed size: 10 bytes)
+    message_data = x.encode('utf-8')
+    message_length = str(len(message_data)).zfill(10).encode('utf-8')
+    chat.send(message_length)
+
+    # Send the actual message
+    chat.send(message_data)
+
+    # Receive the length of the message (fixed size: 10 bytes)
+    length_data = chat.recv(10)
+    length = int(length_data.decode('utf-8'))
+
+    # Receive the message with variable length
+    received_message = chat.recv(length).decode('utf-8')
+    print("Server : " + received_message)
 
 chat.close()
